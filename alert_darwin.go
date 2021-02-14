79a47e3d0ef21926eecd3d8fce09b3619b6638de
Frank Chiarulli Jr <frank@frankchiarulli.com>
2021-02-14 12:16:17 -0500

    move cgo to internal package

--- alert_darwin.go
+++ alert_darwin.go
@@ -1,48 +1,22 @@
 package alert
 
-/*
-#cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework AppKit
-
-#import "alert.h"
-*/
-import "C"
 import (
-	"unsafe"
+	"github.com/fcjr/alert/internal/nsalert"
 )
 
 // Message displays a basic alert with an "OK" button
 func Message(title, message string) error {
-	cTitle := C.CString(title)
-	defer C.free(unsafe.Pointer(cTitle))
-	cMessage := C.CString(message)
-	defer C.free(unsafe.Pointer(cMessage))
-	C.message(cTitle, cMessage)
-	return nil
+	return nsalert.Message(title, message)
 }
 
 // Error displays a basic error alert with an "OK" button
 func Error(title, message string) error {
-	cTitle := C.CString(title)
-	defer C.free(unsafe.Pointer(cTitle))
-	cMessage := C.CString(message)
-	defer C.free(unsafe.Pointer(cMessage))
-	C.error(cTitle, cMessage)
-	return nil
+	return nsalert.Error(title, message)
 }
 
 // Question displays an alert with two buttons
 //
 // returns true iff the the defaultButton was pressed
 func Question(title, message, defaultButton, alternateButton string) (bool, error) {
-	cTitle := C.CString(title)
-	defer C.free(unsafe.Pointer(cTitle))
-	cMessage := C.CString(message)
-	defer C.free(unsafe.Pointer(cMessage))
-	cDefaultButton := C.CString(defaultButton)
-	defer C.free(unsafe.Pointer(cDefaultButton))
-	cAlternateButton := C.CString(alternateButton)
-	defer C.free(unsafe.Pointer(cAlternateButton))
-	ret := C.question(cTitle, cMessage, cDefaultButton, cAlternateButton)
-	return bool(ret), nil
+	return nsalert.Question(title, message, defaultButton, alternateButton)
 }
---
+++ internal/nsalert/alert_darwin.go
@@ -0,0 +1,48 @@
+package nsalert
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework AppKit
+
+#import "alert.h"
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+// Message displays a basic alert with an "OK" button
+func Message(title, message string) error {
+	cTitle := C.CString(title)
+	defer C.free(unsafe.Pointer(cTitle))
+	cMessage := C.CString(message)
+	defer C.free(unsafe.Pointer(cMessage))
+	C.message(cTitle, cMessage)
+	return nil
+}
+
+// Error displays a basic error alert with an "OK" button
+func Error(title, message string) error {
+	cTitle := C.CString(title)
+	defer C.free(unsafe.Pointer(cTitle))
+	cMessage := C.CString(message)
+	defer C.free(unsafe.Pointer(cMessage))
+	C.error(cTitle, cMessage)
+	return nil
+}
+
+// Question displays an alert with two buttons
+//
+// returns true iff the the defaultButton was pressed
+func Question(title, message, defaultButton, alternateButton string) (bool, error) {
+	cTitle := C.CString(title)
+	defer C.free(unsafe.Pointer(cTitle))
+	cMessage := C.CString(message)
+	defer C.free(unsafe.Pointer(cMessage))
+	cDefaultButton := C.CString(defaultButton)
+	defer C.free(unsafe.Pointer(cDefaultButton))
+	cAlternateButton := C.CString(alternateButton)
+	defer C.free(unsafe.Pointer(cAlternateButton))
+	ret := C.question(cTitle, cMessage, cDefaultButton, cAlternateButton)
+	return bool(ret), nil
+}
