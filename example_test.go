45e60858cee87f5b3d824018bca662e6291d94f5
LinkinStar <linkinstar@foxmail.com>
2021-08-19 11:13:25 +0800

    fix encoding problem, use utf8 encoding

--- example_test.go
+++ example_test.go
@@ -2,18 +2,21 @@ package alert_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/fcjr/alert"
 )
 
-func ExampleInfo() {
+func TestExampleInfo(t *testing.T) {
 	_ = alert.Info("Example Message", "Example Message Text")
+	_ = alert.Info("Example 中文标题", "Example 中文消息")
+	_ = alert.Info("Example 테스트 메시지", "Example 테스트 메시지")
 }
-func ExampleError() {
+func TestExampleError(t *testing.T) {
 	_ = alert.Error("Example Error", "Example Error Text")
 }
 
-func ExampleQuestion() {
+func TestExampleQuestion(t *testing.T) {
 	resp, _ := alert.Question("Example Question", "Yes or No?", "Yes", "No")
 	fmt.Printf("Response: %t\n", resp)
 
