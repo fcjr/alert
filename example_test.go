c29e0f9cab10cf8cbff61f3a92c41441fe54aad0
Frank Chiarulli Jr <frank@frankchiarulli.com>
2023-08-16 21:41:33 -0400

    undo changes to example file

--- example_test.go
+++ example_test.go
@@ -2,21 +2,18 @@ package alert_test
 
 import (
 	"fmt"
-	"testing"
 
 	"github.com/fcjr/alert"
 )
 
-func TestExampleInfo(t *testing.T) {
+func ExampleInfo() {
 	_ = alert.Info("Example Message", "Example Message Text")
-	_ = alert.Info("Example 中文标题", "Example 中文消息")
-	_ = alert.Info("Example 테스트 메시지", "Example 테스트 메시지")
 }
-func TestExampleError(t *testing.T) {
+func ExampleError() {
 	_ = alert.Error("Example Error", "Example Error Text")
 }
 
-func TestExampleQuestion(t *testing.T) {
+func ExampleQuestion() {
 	resp, _ := alert.Question("Example Question", "Yes or No?", "Yes", "No")
 	fmt.Printf("Response: %t\n", resp)
 
